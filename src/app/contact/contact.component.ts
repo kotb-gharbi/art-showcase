abfd0ca43a6648850484db0df2751dce5f9efa69
kotb41 <gharbikotb41@gmail.com>
2024-03-27 23:02:15 +0100

    added signup post request + contact us ui

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+
+    OnContactSubmit() {
+      console.log("contact submitted");
+      
+    }
 
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,13 +1,33 @@
+import { HttpClient  } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,FormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+    
+
+    constructor(private http : HttpClient) {}
+
+    OnSignupSubmit(Signup : {Username : String , Email : String , Password : String , type : String}) {
+      console.log(Signup);
+      this.http.post("http://localhost/api/signup.php", Signup).subscribe((res) => {
+    console.log(res);
+    Swal.fire({
+      title: "Good job!",
+      text: "User created successfully!",
+      icon: "success"
+    });
+    },
+    (error) => {
+    console.error("Error:", error);
+    });}
   
 }
