2c2ae61d5432a1ac96074b37c272131ece7f4616
kotb41 <gharbikotb41@gmail.com>
2024-03-31 23:23:25 +0100

    added authservice

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit} from '@angular/core';
 import { FormsModule,FormGroup, ReactiveFormsModule, FormControl , Validators} from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import Swal from 'sweetalert2';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +17,7 @@ export class LoginComponent implements OnInit {
 
   login : FormGroup = new FormGroup({});
 
-  constructor(private http:HttpClient,private router:Router){}
+  constructor(private http:HttpClient,private router:Router,private authService: AuthService){}
 
   ngOnInit(): void {
       this.login= new FormGroup({
@@ -28,12 +29,12 @@ export class LoginComponent implements OnInit {
 
   OnLoginSubmit() {
     if(this.login.valid){
-      this.http.post("http://localhost/api/login.php",this.login.value).subscribe((res : any) =>{
+      this.authService.login(this.login.value).subscribe((res : any) =>{
         console.log(res);
-        if(res.result =="true"){
+        if(res.result){
           this.router.navigate(['/explore']);
         }
-        else if(res.result =="Username incorrect"){
+        else if(res.result =="Username is incorrect"){
           this.login.get("username")?.setErrors({ incorrect: true });
         }
         else if(res.result =="Password incorrect"){
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  
+  private loggedIn = new BehaviorSubject<boolean>(false);
+
+  constructor(private http: HttpClient) { }
+
+  login(user: { username: string, password: string }): Observable<any> {
+    return this.http.post<any>("http://localhost/api/login.php",user).pipe(
+      tap(response => {
+        if (response.result && response.jwt) {
+          localStorage.setItem('token', response.jwt);
+          this.loggedIn.next(true);
+        }
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.loggedIn.next(false);
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    return this.loggedIn.asObservable();
+  }
+}
